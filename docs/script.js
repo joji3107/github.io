4151e948ebf96420f354428fa1a59c706902b2a6
joji3107 <99350946+joji3107@users.noreply.github.com>
2023-03-01 16:42:38 -0300

    Add files via upload

---
+++ docs/script.js
@@ -0,0 +1,14 @@
+function cambio(){
+    document.body.style.background = "pink";
+    document.body.style.color = "green";
+}
+function cambio2(){
+    document.body.style.background = "burlywood";
+    document.body.style.color = "black";
+}
+/**function mostrar(){
+ * document.getElementsByClassName("thhid").style.display = "block";
+ * } */
+/** function hide(){
+    document.getElementById("tabla1").id ="tablas3"
+}*/
